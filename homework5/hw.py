0dabc21c7f0964bddba851eba502c4588d1e02f2
DUCKBR3AD <76643973+DUCKBR3AD@users.noreply.github.com>
2024-10-05 14:16:59 +0500

    Update hw.py

--- homework5/hw.py
+++ homework5/hw.py
@@ -1,1 +1,16 @@
+sum = 0
+
+while True:
+    inp = input("Введите число или 'stop' или 'end' для завершения ввода: ")
+
+    if inp == "stop" or inp =="end"  :
+        break
+
+    try:
+        num = float(inp)
+        sum += num
+    except ValueError:
+        print("Ошибка: введите корректное число ")
+
+print("Сумма введённых чисел: ", sum)
 
