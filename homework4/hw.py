b5119ff137d6dd39227be065a13bd36e2a778852
DUCKBR3AD <76643973+DUCKBR3AD@users.noreply.github.com>
2024-10-05 14:41:07 +0500

    Update hw.py

--- homework4/hw.py
+++ homework4/hw.py
@@ -1,5 +1,9 @@
 a = int(input("Введите значение нижней границы: "))
 b = int(input("Введите значение верхней границы: "))
+try:
+    except ValueError:
+        print("Ошибка:некорректный ввод")
+        break
 if a > b:
     for i in range(a+1,b):
         print(i)
