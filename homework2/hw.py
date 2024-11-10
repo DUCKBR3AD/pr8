c0ebcf0713c04a63cfe74025897b9533e6527af0
DUCKBR3AD <76643973+DUCKBR3AD@users.noreply.github.com>
2024-11-10 16:58:57 +0500

    little bugfix

--- homework2/hw.py
+++ homework2/hw.py
@@ -1,5 +1,7 @@
 while True:
-    a = int(input("Введите первое число: "))
-    b = int(input("Введите второе число: "))
-    print(a+b)
-
+    try:
+        a = int(input("Введите первое число: "))
+        b = int(input("Введите второе число: "))
+        print(f'{a} + {b} =',a+b)
+    except ValueError:
+        print("Некорректный ввод. Пожалуйста, введите число.")
